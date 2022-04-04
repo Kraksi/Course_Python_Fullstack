a1edd800df95fa70062a88837da3a23d2c18b0c4
krasti <krasnov.d.a.20@mail.ru>
2022-04-04 17:18:24 +0300

    first video_ready

--- JavaScript_React_JSX/Book.js
+++ JavaScript_React_JSX/Book.js
@@ -1,34 +1,42 @@
-const Book = (props) => {
-    const h1Style = {
-       color: "#369",
-       fontFamily: "sans-serif",
-   }
-   let {title, author, price} = props;
-   const formatPrice = price => price? <b>{price}</b>: <del></del>;
-   
-   return (
-       <div className="book">
-           <h3 style={h1Style}>{title}</h3>
-           <p>Author: {author}</p>
-           <p>Price: {formatPrice(price)}</p>
-      </div>
-       )
-}
-//class Book extends React.Component {
-//    render (){
+//const Book = (props) => {
 //    const h1Style = {
-//        color: "#369",
-//        fontFamily: "sans-serif",
-//    }
-//    let {title, author, price} = this.props;
-//    const formatPrice = price => price? <b>{price}</b>: <del></del>;
-//    
-//    return (
-//        <div className="book">
-//            <h3 style={h1Style}>{title}</h3>
-//            <p>Author: {author}</p>
-//            <p>Price: {formatPrice(price)}</p>
-//        </div>
-//        )
-//    }
+//       color: "#369",
+//       fontFamily: "sans-serif",
+//   }
+//   let {title, author, price} = props;
+//   const formatPrice = price => price? <b>{price}</b>: <del></del>;
+//   
+//   return (
+//       <div className="book">
+//           <h3 style={h1Style}>{title}</h3>
+//           <p>Author: {author}</p>
+//           <p>Price: {formatPrice(price)}</p>
+//      </div>
+//       )
 //}
+class Book extends React.Component {
+    constructor(props){
+        super(props);
+        this.state ={
+            selected: true
+        }
+    }
+    render (){
+    const h1Style = {
+        color: "#369",
+        fontFamily: "sans-serif",
+    }
+    let {title, author, price} = this.props;
+    const formatPrice = price => price? <b>{price}</b>: <del></del>;
+    const statusBook = (this.state.selected ? 'book-selected' : 'book-default');
+    return (
+        <div className={"book" + statusBook}>
+            <h3 style={h1Style}>{title}</h3>
+            <p>Author: {author}</p>
+            <p>Price: {formatPrice(price)}</p>
+            <a href="#" className = "btn btn-success" onClick={()=>{this.setState({selected: !this.stateselected})}}>Сравнить</a> &nbsp;
+            <a href="#" className = "btn btn-success">В корзину</a>
+        </div>
+        )
+    }
+}
