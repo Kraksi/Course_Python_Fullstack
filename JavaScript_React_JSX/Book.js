0e5ca46410ead7e5c98b81793b637ccca311ede8
krasti <krasnov.d.a.20@mail.ru>
2022-04-04 15:47:00 +0300

    add props

---
+++ JavaScript_React_JSX/Book.js
@@ -0,0 +1,16 @@
+const Book = (props) => {
+    const h1Style = {
+        color: "#369",
+        fontFamily: "sans-serif",
+    }
+    let {title, author, price} = props;
+    const formatPrice = price => price? <b>{price}</b>: <del></del>;
+    
+    return (
+        <div className="book">
+            <h3 style={h1Style}>{title}</h3>
+            <p>Author: {author}</p>
+            <p>Price: {formatPrice(price)}</p>
+        </div>
+        )
+}
