9cd3c41901553606ebae448f91f05c2936042603
krasti <krasnov.d.a.20@mail.ru>
2022-04-04 15:00:07 +0300

    books

--- JavaScript_React_JSX/data.js
+++ JavaScript_React_JSX/data.js
@@ -1,5 +1,5 @@
 const dataBook = [
-    {id:1, title: "React.js. ", author: "Noname1"},
-    {id:5, title: "React & Redux", author: "Noname2"},
-    {id:6, title: "The Road to learn React", author: "Noname3"}
+    {id:1, title: "React.js. ", author: "Stoyan Stefanov", price: 300},
+    {id:5, title: "React & Redux", author: "Noname2", price: 400},
+    {id:6, title: "The Road to learn React", author: "Noname3", price: 700}
 ]
