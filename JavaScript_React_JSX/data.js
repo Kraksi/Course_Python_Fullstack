fdb5fbbaa65d7745aa69ec5c6cd111d94ac0e2cf
krasti <krasnov.d.a.20@mail.ru>
2022-04-04 14:32:32 +0300

    data.js

---
+++ JavaScript_React_JSX/data.js
@@ -0,0 +1,5 @@
+const dataBook = [
+    {id:1, title: "React.js. ", author: "Noname1"},
+    {id:5, title: "React & Redux", author: "Noname2"},
+    {id:6, title: "The Road to learn React", author: "Noname3"}
+]
