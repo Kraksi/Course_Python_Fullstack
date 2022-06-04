a92ba8b2a01773fb20f24d276807b920f3fb5190
Kraksi <krasnov.d.a.20@mail.ru>
2022-06-04 12:37:15 +0300

    add redux

--- JavaScript_Redux_react_router/my-app/src/index.js
+++ JavaScript_Redux_react_router/my-app/src/index.js
@@ -7,6 +7,44 @@ import reportWebVitals from './reportWebVitals';
 //import dataBook from './components/data';
 import dataBook from './components/data.json';
 
+import { createStore, combineReducers } from 'redux'
+
+function counterReducer(state = { value: 0 }, action) {
+  switch (action.type) {
+    case 'counter/incremented':
+      return { value: state.value + 1 }
+    case 'counter/decremented':
+      return { value: state.value - 1 }
+    default:
+      return state
+  }
+}
+
+function textReducer(state = { text: 0 }, action) {
+  switch (action.type) {
+    case 'text/add+':
+      return { text: state.text + '+' }
+    case 'counter/decremented':
+      return { text: state.text + '-' }
+    default:
+      return state
+  }
+}
+
+let store = createStore(counterReducer, textReducer)
+
+store.subscribe(() => console.log(store.getState()))
+
+store.dispatch({ type: 'counter/incremented' })
+// {value: 1}
+store.dispatch({ type: 'counter/incremented' })
+// {value: 2}
+store.dispatch({ type: 'counter/decremented' })
+// {value: 1}
+store.dispatch({ type: 'text/add+' })
+store.dispatch({ type: 'text/add+' })
+store.dispatch({ type: 'text/add-' })
+
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
