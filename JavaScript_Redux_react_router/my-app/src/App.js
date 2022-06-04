efb0f43e38eb36debd87a9edd78d0a0f93f93c72
Kraksi <krasnov.d.a.20@mail.ru>
2022-06-04 12:18:55 +0300

    add last lab and reactstrap

--- JavaScript_Redux_react_router/my-app/src/App.js
+++ JavaScript_Redux_react_router/my-app/src/App.js
@@ -1,14 +1,82 @@
+import React from 'react';
 import logo from './logo.svg';
 import './App.css';
-import Button from './components/Button.js';
+//import Button from './components/Button.js';
+import Basket from './components/Basket';
+import BookWithoutPrice from './components/BookWithoutPrice';
+import Footer from './components/Footer';
+import SearchForm from './components/SearchForm';
+import AddBookForm from './components/AddBookForm';
+import Book from './components/Book';
+import { Button } from 'reactstrap';
 
-function App() {
-  return (
-    <div className="App">
-      Привет, мир!
-      <Button value="Поехали!"/>
-    </div>
-  );
+class App extends React.Component{
+ 
+  constructor(props){
+    super(props);
+    this.state = {
+      dataBook: this.props.dataBook,
+      items: {}
+    };
+    this.addBook = this.addBook.bind(this); 
+    this.addBasket = this.addBasket.bind(this); 
+    this.removeBasket = this.removeBasket.bind(this); 
+  }
+  
+   removeBasket(id){
+     let items = Object.assign({},this.state.items);
+     delete items[id]; 
+     console.log(items);
+     this.setState({ items: items});
+   }  
+   addBasket(id){
+     let items = Object.assign({},this.state.items);
+     items[id] = (id in items) ? items[id]+1 : 1;
+     console.log(items);
+     this.setState({ items: items});
+   }
+ 
+  addBook(book){
+   //alert(JSON.stringify(book))
+   
+   this.setState( prev =>{
+    return {dataBook:prev.dataBook.concat(book)}
+   });
+   
+//    let toAdd = Object.assign({},book);
+//    this.setState({
+//      dataBook: [...this.state.dataBook, toAdd]
+//    }); 
+
+  }
+ 
+  render(){
+    const book = this.state.dataBook. map( item => (                         
+    item["price"] ?   <Book
+    id={item["id"]}
+    key={item["id"]}
+    title={item["title"]}
+    author={item["author"]}
+    price={item["price"]}
+    handleAddBasket={this.addBasket}
+    /> :
+    <BookWithoutPrice
+    key={item["id"]}
+    title={item["title"]}
+    author={item["author"]}
+    price={item["price"]}
+    />
+    )
+    )
+    return <div>
+      <Button color="danger">Danger!</Button>
+      <Button color="success">Success!</Button>
+      <Basket items={this.state.items} dataBook={this.state.dataBook} handleRemoveBasket={this.removeBasket} />
+      <AddBookForm handleAddBook={this.addBook}/>
+      <SearchForm />
+     {book}
+    </div>;
+  }
 }
 
 export default App;
--- JavaScript_Redux_react_router/my-app/src/index.js
+++ JavaScript_Redux_react_router/my-app/src/index.js
@@ -1,13 +1,16 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.css';
+import 'bootstrap/dist/css/bootstrap.min.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
+//import dataBook from './components/data';
+import dataBook from './components/data.json';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
-    <App />
+    <App dataBook={dataBook} />
   </React.StrictMode>
 );
 
