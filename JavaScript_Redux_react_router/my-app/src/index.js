7d462fd50d3ee00b26455b8174ddb7e0ce65eeb7
Kraksi <krasnov.d.a.20@mail.ru>
2022-06-04 19:26:42 +0300

    add actions

--- JavaScript_Redux_react_router/my-app/src/index.js
+++ JavaScript_Redux_react_router/my-app/src/index.js
@@ -7,43 +7,85 @@ import reportWebVitals from './reportWebVitals';
 //import dataBook from './components/data';
 import dataBook from './components/data.json';
 
+//import { createStore, combineReducers } from 'redux'
+
+//function counterReducer(state = { value: 0 }, action) {
+//  switch (action.type) {
+//    case 'counter/incremented':
+//      return { value: state.value + 1 }
+//    case 'counter/decremented':
+//      return { value: state.value - 1 }
+//    default:
+//      return state
+//  }
+//}
+
+//function textReducer(state = { text: 0 }, action) {
+//  switch (action.type) {
+//    case 'text/add+':
+//      return { text: state.text + '+' }
+//    case 'counter/decremented':
+//      return { text: state.text + '-' }
+//    default:
+ //     return state
+//  }
+//}
+
+//let store = createStore(counterReducer, textReducer)
+
+//store.subscribe(() => console.log(store.getState()))
+
+//store.dispatch({ type: 'counter/incremented' })
+// {value: 1}
+//store.dispatch({ type: 'counter/incremented' })
+// {value: 2}
+//store.dispatch({ type: 'counter/decremented' })
+// {value: 1}
+//store.dispatch({ type: 'text/add+' })
+//store.dispatch({ type: 'text/add+' })
+//store.dispatch({ type: 'text/add-' })
+
 import { createStore, combineReducers } from 'redux'
 
-function counterReducer(state = { value: 0 }, action) {
+function SReducer(state = 0, action) {
   switch (action.type) {
-    case 'counter/incremented':
-      return { value: state.value + 1 }
-    case 'counter/decremented':
-      return { value: state.value - 1 }
+    case 'set/a':
+      return action.value
     default:
       return state
   }
 }
 
-function textReducer(state = { text: 0 }, action) {
+function pReducer(state = 0, action) {
   switch (action.type) {
-    case 'text/add+':
-      return { text: state.text + '+' }
-    case 'counter/decremented':
-      return { text: state.text + '-' }
+    case 'set/p':
+      return action.value
     default:
       return state
   }
 }
 
-let store = createStore(counterReducer, textReducer)
+function nReducer(state = 0, action) {
+  switch (action.type) {
+    case 'set/n':
+      return action.value
+    default:
+      return state
+  }
+}
+let preloadedState = JSON.parse(localStorage.getItem('mortgage')) ?? {S:1e6, p:7, n:10};
 
-store.subscribe(() => console.log(store.getState()))
+let store = createStore(combineReducers({S:SReducer,p:pReducer,n:nReducer}),preloadedState)
 
-store.dispatch({ type: 'counter/incremented' })
-// {value: 1}
-store.dispatch({ type: 'counter/incremented' })
-// {value: 2}
-store.dispatch({ type: 'counter/decremented' })
-// {value: 1}
-store.dispatch({ type: 'text/add+' })
-store.dispatch({ type: 'text/add+' })
-store.dispatch({ type: 'text/add-' })
+store.subscribe(() => {
+  console.log(store.getState())
+  localStorage.setItem('mortgage', JSON.stringify(store.getState))
+})
+
+store.dispatch({ type: 'set/s', value: 1_000_00 })
+store.dispatch({ type: 'set/p', value: 10 })
+store.dispatch({ type: 'set/n', value: 15 })
+setTimeout(()=>{store.dispatch({type: 'set/s', value: 5_000_000})}, 5000)
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
