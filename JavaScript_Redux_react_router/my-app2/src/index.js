bc7100e1a934c504dd65fdb66928cb10355cd91d
Kraksi <krasnov.d.a.20@mail.ru>
2022-06-05 13:00:40 +0300

    add routes about/basket/catalog

--- JavaScript_Redux_react_router/my-app2/src/index.js
+++ JavaScript_Redux_react_router/my-app2/src/index.js
@@ -3,12 +3,30 @@ import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
+import {
+  BrowserRouter,
+  Routes,
+  Route
+} from 'react-router-dom';
+
+import Catalog from './routes/catalog';
+import Basket from './routes/basket';
+import About from './routes/about';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
-    <App />
+    <BrowserRouter>
+    <Routes>
+    <Route path="/" element={<App />}>
+    <Route path="catalog" element={<Catalog />}/>
+    <Route path="basket" element={<Basket />}/>
+    <Route path="about" element={<About />}/>
+    </Route>
+    </Routes>
+    </BrowserRouter>
   </React.StrictMode>
+  
 );
 
 // If you want to start measuring performance in your app, pass a function
